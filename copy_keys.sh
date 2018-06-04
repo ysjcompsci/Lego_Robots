42f6dc99224d73a8e46c93e520cc680233b462ed
Carl Hetherington <cth@carlh.net>
2018-06-04 11:46:14 +0100

    Same for copy_keys.sh.

--- copy_keys.sh
+++ copy_keys.sh
@@ -2,5 +2,8 @@
 if [ ! -f "$HOME/.ssh/id_rsa.pub" ]; then
     ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa
 fi
-sudo ifconfig enp0s26u1u4 10.42.0.1 up
+
+device=$(cat /proc/net/dev | grep enp0 | awk '{print $1}' | sed -e "s/://g")
+sudo ifconfig $device 10.42.0.1 up
+
 ssh-copy-id robot@10.42.0.3
